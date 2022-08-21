910f369632be8fdd146cecff95a690cd42c53b0f
Saurav Vaidya <svaidya.se@gmail.com>
2022-08-21 06:06:50 -0700

    Updates

---
+++ precache-manifest.f2ddf816938630cf8e9e890d9bde5353.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc61083b11ba9829ec4dbfe45d7dda06",
+    "url": "/Notes-maker/index.html"
+  },
+  {
+    "revision": "f5115e9113d34e11d7de",
+    "url": "/Notes-maker/static/js/2.88e6856e.chunk.js"
+  },
+  {
+    "revision": "df53c41b755e0056129a",
+    "url": "/Notes-maker/static/js/main.7fc6fa1a.chunk.js"
+  },
+  {
+    "revision": "6beb37e72e489767b5ec",
+    "url": "/Notes-maker/static/js/runtime-main.14b61b2b.js"
+  }
+]);
